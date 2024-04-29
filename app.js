98b492e7df5a248c05cfff0bfb8ad1f2d3a41fda
unknown <a27657060@gmail.com>
2024-04-29 13:46:12 +0800

    feat: router init & hbs init

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express')
+const router = require('./routes')
 const app = express()
-const { engine } = express.Router()
+const { engine } = require('express-handlebars')
 
 app.engine('hbs', engine({ extname: 'hbs' }))
 app.set('view engine', 'hbs')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(router)
 
 app.listen(3000, () => {
-  console.log('Example app listening on http://localhost:3000!')
+  console.log('Example app listening on http://localhost:3000')
 })
