3ce89add2d993d7240a4f1c1473fed1e0b01f6b5
unknown <a27657060@gmail.com>
2024-05-02 22:01:43 +0800

    feat: implement search for home page

--- controllers/home-controllers.js
+++ controllers/home-controllers.js
@@ -1,4 +1,5 @@
 const { Teacher, User } = require('../models')
+const { Op } = require('sequelize')
 const { getOffset, getPagination } = require('../helpers/pagination-helpers')
 
 const homeControllers = {
@@ -6,8 +7,21 @@ const homeControllers = {
     const limit = 6
     const page = Number(req.query.page) || 1
     const offset = getOffset(page, limit)
+
+    const keyword = req.query.keyword?.trim() || ''
+    const searchCondition = keyword
+      ? {
+          [Op.or]: [
+            { '$User.name$': { [Op.like]: `%${keyword}%` } },
+            { course_description: { [Op.like]: `%${keyword}%` } },
+            { '$User.nation$': { [Op.like]: `%${keyword}%` } }
+          ]
+        }
+      : {}
+
     Teacher.findAndCountAll({
       include: [User],
+      where: searchCondition,
       raw: true,
       nest: true,
       offset,
@@ -21,7 +35,7 @@ const homeControllers = {
             courseDescription: teacher.courseDescription.length > 100 ? teacher.courseDescription.substring(0, 100) + '...' : teacher.courseDescription
           }
         ))
-        return res.render('home', { teachers: teachersData, page, pagination: getPagination(page, limit, teachers.count) })
+        return res.render('home', { teachers: teachersData, page, pagination: getPagination(page, limit, teachers.count), keyword })
       })
       .catch(err => next(err))
   }
