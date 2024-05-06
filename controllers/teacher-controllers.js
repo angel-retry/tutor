2fe075cef795330c8c450ac7d7b85d6814ef4ea9
unknown <a27657060@gmail.com>
2024-05-06 15:55:46 +0800

    feat: add teacher data for teacher page

--- controllers/teacher-controllers.js
+++ controllers/teacher-controllers.js
@@ -1,9 +1,40 @@
+const { Teacher, User, Lesson, Rating, Sequelize } = require('../models')
+const { Op } = require('sequelize')
+
 const teacherControllers = {
   getTeacherCreatePage: (req, res) => {
     return res.render('teacher-create')
   },
-  getTeacherPage: (req, res) => {
-    return res.render('teacher')
+  getTeacherPage: (req, res, next) => {
+    const teacherId = Number(req.params.id)
+    if (teacherId !== req.user.id) throw new Error('沒有權限看此資料!')
+    Teacher.findByPk(teacherId, {
+      include: [
+        {
+          model: Lesson,
+          include: [{ model: User, attributes: ['id', 'name'] }],
+          where: {
+            startTime: {
+              [Op.gt]: new Date()
+            }
+          }
+        },
+        Rating
+      ],
+      attributes: {
+        include: [
+          [
+            Sequelize.literal('(SELECT ROUND(AVG(rating), 1) FROM Ratings WHERE Ratings.teacher_id = Teacher.id)'), 'totalRating'
+          ]
+        ]
+      }
+    })
+      .then(teacher => {
+        teacher = teacher.toJSON()
+        console.log('teacher', teacher)
+        return res.render('teacher', { teacher })
+      })
+      .catch(err => next(err))
   },
   getTeacherEditPage: (req, res) => {
     return res.render('teacher-edit')
