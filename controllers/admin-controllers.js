9c3582b4ee59f294ec60df273e42bf12e7b1872c
unknown <a27657060@gmail.com>
2024-05-09 13:22:58 +0800

    feat: add users data for admin page

--- controllers/admin-controllers.js
+++ controllers/admin-controllers.js
@@ -1,6 +1,20 @@
+const { getOffset, getPagination } = require('../helpers/pagination-helpers')
+const { User } = require('../models')
+
 const adminControllers = {
   getAdminPage: (req, res) => {
-    return res.render('admin/admin')
+    const limit = 20
+    const page = Number(req.query.page) || 1
+    const offset = getOffset(page, limit)
+    User.findAndCountAll({
+      attributes: ['id', 'name', 'email', 'isTeacher'],
+      raw: true,
+      offset,
+      limit
+    })
+      .then(users => {
+        return res.render('admin/admin', { users: users.rows, page, pagination: getPagination(page, limit, users.count) })
+      })
   }
 }
 
