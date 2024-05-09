53fb90c498d00be1fe204912bf2649ae50efee1a
unknown <a27657060@gmail.com>
2024-05-09 13:46:21 +0800

    feat: implement search for admin page

--- controllers/admin-controllers.js
+++ controllers/admin-controllers.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize')
 const { getOffset, getPagination } = require('../helpers/pagination-helpers')
 const { User } = require('../models')
 
@@ -6,14 +7,35 @@ const adminControllers = {
     const limit = 20
     const page = Number(req.query.page) || 1
     const offset = getOffset(page, limit)
+    const keyword = req.query.keyword?.trim() || ''
+    let searchCondition = {}
+    if (keyword === '學生') {
+      searchCondition = {
+        isTeacher: false
+      }
+    } else if (keyword === '老師') {
+      searchCondition = {
+        isTeacher: true
+      }
+    } else if (keyword) {
+      searchCondition =
+        {
+          [Op.or]: [
+            { id: { [Op.like]: `${keyword}` } },
+            { name: { [Op.like]: `${keyword}` } },
+            { email: { [Op.like]: `${keyword}` } }
+          ]
+        }
+    }
     User.findAndCountAll({
       attributes: ['id', 'name', 'email', 'isTeacher'],
       raw: true,
       offset,
-      limit
+      limit,
+      where: searchCondition
     })
       .then(users => {
-        return res.render('admin/admin', { users: users.rows, page, pagination: getPagination(page, limit, users.count) })
+        return res.render('admin/admin', { users: users.rows, page, pagination: getPagination(page, limit, users.count), keyword })
       })
   }
 }
