e0e86da387567b24f3dad38f1826bb3d6475e7e5
unknown <a27657060@gmail.com>
2024-06-28 10:10:51 +0800

    feat: refactor multer

--- middlewares/multer-helpers.js
+++ middlewares/multer-helpers.js
@@ -1,4 +1,18 @@
+const path = require('path')
+
 const multer = require('multer')
-const upload = multer({ dest: 'temp/' })
+const upload = multer({
+  dest: '/tem/upload',
+  limits: {
+    fileSize: 1024 * 1024 * 2
+  },
+  fileFilter (req, file, cb) {
+    const ext = path.extname(file.originalname).toLowerCase()
+    if (ext !== '.jpg' && ext !== '.png' && ext !== '.jpeg') {
+      cb(new Error('檔案格式錯誤，僅限上傳 jpg、jpeg、png 與 gif 格式。'))
+    }
+    cb(null, true)
+  }
+})
 
 module.exports = upload
